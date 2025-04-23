b313a2dee28080b4cccdf37e830a32babca952c3
inafu1137 <inafu1137@gmail.com>
2025-04-23 10:17:06 +0900

    ESLintの修正

--- 02.calendar/eslint.config.js
+++ 02.calendar/eslint.config.js
@@ -1,28 +1,9 @@
-// eslint.config.js
-import js from "@eslint/js";
 import globals from "globals";
-import { createRequire } from "node:module";
-
-const require = createRequire(import.meta.url);
-const eslintConfigPrettier = require("eslint-config-prettier");
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        ...globals.node,
-        ...globals.es2021,
-      },
-    },
-    plugins: {
-      js,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-    },
-  },
+  { languageOptions: { globals: globals.nodeBuiltin } },
+  pluginJs.configs.recommended,
   eslintConfigPrettier,
 ];
