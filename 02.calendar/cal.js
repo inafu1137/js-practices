bbd825aea2fa71f96b6ebd6f6d439cf758a97c70
inafu1137 <inafu1137@gmail.com>
2025-05-13 15:07:05 +0900

    コメントの削除

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -8,24 +8,20 @@ const now = dayjs();
 const year = Number.isInteger(options.y) ? options.y : now.year();
 const month = Number.isInteger(options.m) ? options.m : now.month() + 1;
 
-// 入力チェック
 if (year < 1970 || year > 2100 || month < 1 || month > 12) {
   console.error("有効な範囲は年: 1970〜2100、月: 1〜12です。");
   process.exit(1);
 }
 
-// 月初オブジェクト
 const startDate = dayjs()
   .set("year", year)
   .set("month", month - 1)
   .set("date", 1);
 
-// 見出しの表示
 const header = `${month}月 ${year}`;
 console.log(header.padStart(13));
 console.log("日 月 火 水 木 金 土");
 
-// カレンダーの出力
 let line = " ".repeat(startDate.day() * 3);
 for (let date = 1; date <= startDate.endOf("month").date(); date++) {
   const current = startDate.set("date", date);
